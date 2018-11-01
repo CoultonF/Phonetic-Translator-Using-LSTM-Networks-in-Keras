0d5fe6b8d30ce18c861191135dba692eb96211a0
CoultonF <cjrfraser@gmail.com>
2018-10-31 23:50:14 -0600

    worked on getting one hot encoding into data structure

--- cgan.py
+++ cgan.py
@@ -1,9 +1,7 @@
 import pandas
 from pandas import *
 import string
-import pprint
-
-pp = pprint.PrettyPrinter(indent=1)
+widx = 0
 fields = ['word', 'ipa']
 words = []
 IPA = []
@@ -16,13 +14,33 @@ for x in df.word:
 for y in df.ipa:
     IPA.append(y)
 
-print(words[100])
-print(IPA[100])
+print(words[widx])
+print(IPA[widx])
 
-def string_vectorizer(strng, alphabet=string.ascii_lowercase + ' .-'):
-    vector = [[0 if char != letter else 1 for char in alphabet]
-                  for letter in strng]
+def unique(list1):
+
+    # intilize a null list
+    unique_list = []
+
+    # traverse for all elements
+    for x in list1:
+        # check if exists in unique_list or not
+        for letter in x:
+            if letter not in unique_list:
+                unique_list.append(letter)
+
+    # print list
+    for x in unique_list:
+        print x,
+print("the unique values from 1st list is")
+unique(words)
+
+def string_vectorizer(str_input, alphabet):
+    vector = [[0 if char != symbol else 1 for char in alphabet]
+                  for symbol in str_input]
     return vector
-one_hot = []
-one_hot = string_vectorizer(words[100])
-print DataFrame(one_hot)
+
+print (string_vectorizer(words[widx], string.ascii_lowercase + '.-'))
+print (string_vectorizer(words[widx], string.ascii_lowercase + '.-'))
+X = tf.placeholder(dtype=float32, Shape=[None, 28])
+# Y=
