cf02c27c1f82fd606deb91191923e9a0a40243b3
Coultonf <cjrfraser@gmail.com>
2018-11-01 18:24:10 -0600

    finished data cleaning and input to a one hot

--- cgan.py
+++ cgan.py
@@ -1,11 +1,12 @@
 import pandas
+import tensorflow as tf
 from pandas import *
 import string
 widx = 0
 fields = ['word', 'ipa']
 words = []
 IPA = []
-df = pandas.read_csv('non-name-words.txt', encoding='utf-16-le', usecols=fields)
+df = pandas.read_csv('name-words.txt', encoding='utf-16-le', usecols=fields)
 
 for x in df.word:
     words.append(x)
@@ -23,24 +24,33 @@ def unique(list1):
     unique_list = []
 
     # traverse for all elements
-    for x in list1:
+    for word in list1:
+        print word
         # check if exists in unique_list or not
-        for letter in x:
+        for letter in word:
             if letter not in unique_list:
                 unique_list.append(letter)
 
-    # print list
     for x in unique_list:
         print x,
-print("the unique values from 1st list is")
-unique(words)
+    return unique_list
+
+
+alpha_chars = unique(words)
+print('\n')
+ipa_chars = unique(IPA)
+print('\n')
+print len(alpha_chars)
+print len(ipa_chars)
+
 
 def string_vectorizer(str_input, alphabet):
     vector = [[0 if char != symbol else 1 for char in alphabet]
                   for symbol in str_input]
     return vector
 
-print (string_vectorizer(words[widx], string.ascii_lowercase + '.-'))
-print (string_vectorizer(words[widx], string.ascii_lowercase + '.-'))
-X = tf.placeholder(dtype=float32, Shape=[None, 28])
-# Y=
+print(string_vectorizer(words[widx], alpha_chars))
+print(string_vectorizer(IPA[widx], ipa_chars))
+
+X = tf.placeholder(dtype=tf.float32, shape=[None, len(alpha_chars)])
+Y = tf.placeholder(dtype=tf.float32, shape=[None, len(ipa_chars)])
