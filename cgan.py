1f04ade9ee711da866d31c9340499711bb8de210
CoultonF <cjrfraser@gmail.com>
2018-11-02 03:40:27 -0600

    char counts

--- cgan.py
+++ cgan.py
@@ -27,8 +27,6 @@ train_df = pandas.read_csv('non-name-words.txt', encoding='utf-16-le', usecols=f
 
 for x in train_df.word:
     train_words.append(x)
-
-
 for y in train_df.ipa:
     train_ipa.append(y)
 
@@ -38,8 +36,6 @@ test_df = pandas.read_csv('name-words.txt', encoding='utf-16-le', usecols=fields
 
 for x in test_df.word:
     test_words.append(x)
-
-
 for y in test_df.ipa:
     test_ipa.append(y)
 
@@ -48,9 +44,10 @@ train_ipa_chars = unique(train_ipa)
 
 print("Character Vocab")
 print(*train_alpha_chars, sep=',')
+print(len(train_alpha_chars))
 print("IPA Vocab")
 print(*train_ipa_chars, sep=',')
-
+print(len(train_ipa_chars))
 
 # x_train and y_train data structure
 # [ [
