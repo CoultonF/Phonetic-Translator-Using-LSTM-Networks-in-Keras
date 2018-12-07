5e322c87709196b4f9bbc9d3e1dd80be90d875d9
Coultonf <cjrfraser@gmail.com>
2018-12-06 22:01:37 -0700

    tried different data sizes

--- data/manipulation.py
+++ data/manipulation.py
@@ -1,14 +1,14 @@
 import pandas
 from pandas import *
 fields = ['word', 'ipa']
-df = pandas.read_csv('small-name-words-small-dataset2.txt', encoding='utf-8', usecols=fields)
+df = pandas.read_csv('non-name-words.txt', encoding='utf-16-le', usecols=fields)
 
 drop_idxs = []
 for idx, row in df.iterrows():
     exclude = set('-.')
-    if len(row.word) > 9 or len(row.ipa) > 9 or idx%3==0 or (any((c in exclude) for c in row.word)):
+    if len(row.word) > 5 or len(row.ipa) > 5 or (any((c in exclude) for c in row.word)):
         drop_idxs.append(idx)
 
 # print(drop_idxs)
 df = df.drop(drop_idxs, axis=0)
-df.to_csv(r'./small-name-words-small-dataset3.txt',header=None, index=None, mode='a',sep=',')
+df.to_csv(r'./5-letter-non-name-words-dataset.txt',encoding='utf-16-le', header=None, index=None, mode='a',sep=',')
